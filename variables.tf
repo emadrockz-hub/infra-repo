c0c012785a4f439f83aefbca4da64480b9b49c4a
emadrockz-hub <emadrockz@gmail.com>
2026-01-21 00:42:31 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,22 +1,18 @@
 variable "project_name" {
-  description = "Name prefix for resources"
   type        = string
   default     = "three-container-app"
 }
 
 variable "aws_region" {
-  description = "AWS region"
   type        = string
   default     = "us-east-1"
 }
 
 variable "my_ip_cidr" {
-  description = "Your public IP in CIDR (set this! e.g. 1.2.3.4/32). Default is open to the world."
   type        = string
   default     = "0.0.0.0/0"
 }
 
 variable "key_name" {
-  description = "Existing AWS EC2 Key Pair name (used for SSH). Create/import one in EC2 console."
-  type        = string
+  type = string
 }
